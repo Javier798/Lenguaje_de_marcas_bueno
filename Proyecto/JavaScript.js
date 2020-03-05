6aa6b0ab79a931de51b25f1dc6ecc9af20b4f354
JavierMarinQuintana <javiermarin798@gmail.com>
2020-03-05 21:40:23 +0100

    Cambios

---
+++ Proyecto/JavaScript.js
@@ -0,0 +1,17 @@
+function myFunction(){
+	if(document.getElementById("listapocha").style.display == "none"){
+		document.getElementById("listapocha").style.display = "block";
+	}
+		else{
+			document.getElementById("listapocha").style.display = "none";
+		}
+	}
+
+	function despliegaLista(){
+	if(document.getElementById("listaDesplegable").style.display == "none"){
+		document.getElementById("listaDesplegable").style.display = "block";
+	}
+		else{
+			document.getElementById("listaDesplegable").style.display = "none";
+		}
+	}
